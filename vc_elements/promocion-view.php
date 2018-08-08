ce925a2382176d69c82cab5d1a844e517a49cd88
Mayco Barale <maycobarale@gmail.com>
2018-08-08 10:42:09 -0300

    fecha de fin de promocion agregada a la vista de la grilla

--- vc_elements/promocion-view.php
+++ vc_elements/promocion-view.php
@@ -1,8 +1,3 @@
-<?php
-/**
- * @package pro
- */
-?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <div class="knx_item_wrapper">
         <?php if(has_post_thumbnail()): ?>
@@ -12,10 +7,13 @@
         <?php endif;?>
         <div class="knx_item_content">
             <h4 class="knx_item_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                <div class='knx_item_excerpt'>
+            <div class="knx_item_meta-data">
+                <p><?php _e('Available until: ', 'knx-promotions');  echo gmdate('d/m/Y', get_post_meta( get_the_ID(), '_knx-promo_valido_hasta', true )); ?></p>
+            </div>
+            <div class='knx_item_excerpt'>
                 <?php the_excerpt(); ?>
-                </div>
-            <a href="<?php the_permalink();?>" class="vc_btn vc_general vc_gitem-link knx_btn"><?php _e('View promotion', 'knx-promotions') ?></a>
+            </div>
+            <a href="<?php the_permalink();?>" class="vc_btn vc_general vc_gitem-link knx_btn"><?php _e('View promotion', 'knx-promotions'); ?></a>
             <?php // Si el VC rompe las clases del child theme quitar las clases vc_* del <a> y definirlas en styles.css ?>
         </div>
         <div class="knx_clearfix"></div>
